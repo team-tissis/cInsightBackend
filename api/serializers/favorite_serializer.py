f96ab0896cbb95d1784f805b600716db184598bc
kato atsushi <ka.baseball1997@gmail.com>
2022-12-06 19:22:24 +0900

    fix favorite serialize

--- api/serializers/favorite_serializer.py
+++ api/serializers/favorite_serializer.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers 
 from api.models import Comment, CustomeUser, Favorite
 from api.serializers.user_serializer import UserSerializer
+from api.serializers.comment_serializer import CommentSerializer
  
  
 class FavoriteSerializer(serializers.ModelSerializer):
-    lecture_id = serializers.IntegerField(required=False)
-    commenter = UserSerializer(many=False, read_only=True)
+    user = UserSerializer(many=False, read_only=True)
+    user_id = serializers.IntegerField(required=False)
+    comment = CommentSerializer(many=False, read_only=True)
     commenter_id = serializers.IntegerField(required=False)
  
     class Meta:
         model = Favorite
         fields = "__all__"
-        read_only_fields = ("id", "code", "created_at", "updated_at")
+        read_only_fields = ("id", "is_synced","vote_weight", "created_at", "updated_at")
